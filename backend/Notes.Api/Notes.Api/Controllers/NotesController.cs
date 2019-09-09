79393d5218103f30164bc8c30f07f47e1c15c062
Mateusz Gajda <matgajda@outlook.com>
2019-09-09 18:42:02 +0200

    Add note migration

--- backend/Notes.Api/Notes.Api/Controllers/NotesController.cs
+++ backend/Notes.Api/Notes.Api/Controllers/NotesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Notes.Api.DataAccess;
+using Notes.Api.Dtos;
 using Notes.Api.Models;
 
 namespace Notes.Api.Controllers
@@ -24,31 +25,32 @@ namespace Notes.Api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Note>> Get()
         {
-            return _dbContext.Notes;
+            return _dbContext.Set<Note>();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<Note> Get(int id)
         {
-            return _dbContext.Notes.SingleOrDefault(note => note.Id == id);
+            return _dbContext.Set<Note>().SingleOrDefault(note => note.Id == id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string title, string content)
+        public void Post([FromBody] NoteDto noteDto)
         {
-            var note = new Note(title, content);
+            var note = new Note(noteDto.Title, noteDto.Content);
             _dbContext.Notes.Add(note);
+            _dbContext.SaveChanges();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string title, string content)
+        public void Put(int id, [FromBody] NoteDto noteDto)
         {
             var note = _dbContext.Notes.SingleOrDefault(n => n.Id == id);
-            note.Title = title;
-            note.Content = content;
+            note.Title = noteDto.Title;
+            note.Content = noteDto.Content;
             _dbContext.Notes.Update(note);
         }
 
